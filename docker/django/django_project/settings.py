dd68310f478e4fe91f5d52e684885e81e3234f08
Olha Shapoval <belyaevaolya0@gmail.com>
2025-09-23 15:47:06 +0300

    updated settings

--- docker/django/django_project/settings.py
+++ docker/django/django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r+8ayl_wm#)yrl%y!c!p@#oso%)igscit+ce!*i)5y5x%qp9eq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
