81698f8cec45aa289f44fb8598f8896f06f6d776
Olha Shapoval <belyaevaolya0@gmail.com>
2025-10-03 11:48:35 +0300

    updated configs

--- modules/jenkins/providers.tf
+++ modules/jenkins/providers.tf
@@ -1,13 +1,7 @@
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
 provider "helm" {
   kubernetes {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_ca)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
