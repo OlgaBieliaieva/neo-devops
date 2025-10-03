901a15f8afceb67a5d2e725610f137fbaba10369
Olha Shapoval <belyaevaolya0@gmail.com>
2025-10-03 11:29:12 +0300

    updated configs

--- modules/argo_cd/providers.tf
+++ modules/argo_cd/providers.tf
@@ -1,13 +1,13 @@
 provider "kubernetes" {
-  host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(var.cluster_ca)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
-  kubernetes = {
-    host                   = var.cluster_endpoint
-    cluster_ca_certificate = base64decode(var.cluster_ca)
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
--- modules/eks/aws_ebs_csi_driver.tf
+++ modules/eks/aws_ebs_csi_driver.tf
@@ -27,15 +27,3 @@ resource "aws_iam_role_policy_attachment" "ebs_irsa_policy" {
   role       = aws_iam_role.ebs_csi_irsa_role.name
 }
 
-resource "aws_eks_addon" "ebs_csi_driver" {
-  cluster_name             = aws_eks_cluster.this.name
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.41.0-eksbuild.1"
-  service_account_role_arn = aws_iam_role.ebs_csi_irsa_role.arn
-  resolve_conflicts_on_update = "PRESERVE"
-
-  depends_on = [
-    aws_iam_openid_connect_provider.oidc,
-    aws_iam_role_policy_attachment.ebs_irsa_policy
-  ]
-}
